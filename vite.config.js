f17ac6a8b4ee2375ea671fae08230a6b54022d20
basiaszafraniec <basia.szafraniec@gmail.com>
2024-12-06 10:30:53 +0100

    nvm did it wrong lol

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import react from "@vitejs/plugin-react-swc"; // this on might be "import react 
 export default defineConfig(({ command }) => {
     const config = {
         plugins: [react()],
-        base: "/raccoony"
+        base: "/"
     };
 
     if (command !== "serve") {
-        config.base = "/react-vite-rest-post-app-with-auth/";
+        config.base = "/raccoony";
     }
 
     return config;
